d52217c5bba634d57767f101fa42b19c5676b612
PongXD <brendanyhy@gmail.com>
2017-03-01 19:02:59 +0800

    Added Andriod Studio project
    Has one page, with one button

---
+++ CS4347Application/app/src/main/java/cs4347group10/cs4347application/MainActivity.java
@@ -0,0 +1,13 @@
+package cs4347group10.cs4347application;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
