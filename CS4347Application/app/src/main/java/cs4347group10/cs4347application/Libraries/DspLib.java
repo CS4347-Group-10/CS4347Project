440cbedc3c8d7b4b3a115ff5fec8e608d27fb627
rodsonchue <warboi@hotmail.com>
2017-03-31 21:43:28 +0800

    Added unit tests. FFT library works!

--- CS4347Application/app/src/main/java/cs4347group10/cs4347application/Libraries/DspLib.java
+++ CS4347Application/app/src/main/java/cs4347group10/cs4347application/Libraries/DspLib.java
@@ -8,20 +8,52 @@ package cs4347group10.cs4347application.Libraries;
  * Examples include Forward/Inverse Fourier Transforms.
  */
 
+import java.lang.Math.*;
 import edu.emory.mathcs.jtransforms.fft.DoubleFFT_1D;
 
 public class DspLib {
+    static public double[] sineWave(int numOfSamples, double magnitude, double frequency, int samplingRate) {
+        double[] samples = new double[numOfSamples];
+        double k = Math.PI * 2. * frequency / samplingRate;
+
+        for (int i = 0; i < numOfSamples; i++){
+            samples[i] = magnitude * Math.sin(k * i);
+        }
+        return samples;
+    }
+
+    /*
+    Computes forward FFT (time-domain to frequency-domain)
+    @return complex FFT values.
+     */
     static public double[] forwardFFT(double[] input) {
         DoubleFFT_1D inputSeq = new DoubleFFT_1D(input.length);
-        double output[] = new double[input.length];
-        inputSeq.realForward(output);
+        double output[] = new double[input.length * 2];
+        System.arraycopy(input, 0, output, 0, input.length);
+        inputSeq.realForwardFull(output);
         return output;
     }
 
     static public double[] inverseFFT(double[] input) {
-        DoubleFFT_1D inputSeq = new DoubleFFT_1D(input.length);
+        DoubleFFT_1D inputSeq = new DoubleFFT_1D(input.length/2);
         double output[] = new double[input.length];
-        inputSeq.realInverse(output, false);
-        return output;
+        System.arraycopy(input, 0, output, 0, input.length);
+        inputSeq.complexInverse(output, true);
+
+        double realOutput[] = new double[input.length/2];
+        //Ignore complex terms, only take real terms
+        for (int i = 0; i < input.length/2; i++){
+            realOutput[i] = output[i * 2];
+        }
+        return realOutput;
+    }
+
+    static public double [] magnitude(double fft[]) {
+        double[] values = new double[fft.length / 2];
+        for (int i = 0; i < fft.length / 2; i++) {
+            int idx = 2 * i;
+            values[i] = Math.sqrt((fft[idx] * fft[idx]) + (fft[idx + 1] * fft[idx + 1]));
+        }
+        return values;
     }
 }
