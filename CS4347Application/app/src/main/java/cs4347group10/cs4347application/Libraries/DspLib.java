d044fff2d519531f49900a0d4ea8f8174d566eea
rodsonchue <warboi@hotmail.com>
2017-03-31 16:23:18 +0800

    Add missing files to project again

---
+++ CS4347Application/app/src/main/java/cs4347group10/cs4347application/Libraries/DspLib.java
@@ -0,0 +1,27 @@
+package cs4347group10.cs4347application.Libraries;
+
+/**
+ * Created by Rodson on 03/26/2017.
+ *
+ * Class contains static methods related to Digital Signal Processing (DSP)
+ *
+ * Examples include Forward/Inverse Fourier Transforms.
+ */
+
+import edu.emory.mathcs.jtransforms.fft.DoubleFFT_1D;
+
+public class DspLib {
+    static public double[] forwardFFT(double[] input) {
+        DoubleFFT_1D inputSeq = new DoubleFFT_1D(input.length);
+        double output[] = new double[input.length];
+        inputSeq.realForward(output);
+        return output;
+    }
+
+    static public double[] inverseFFT(double[] input) {
+        DoubleFFT_1D inputSeq = new DoubleFFT_1D(input.length);
+        double output[] = new double[input.length];
+        inputSeq.realInverse(output, false);
+        return output;
+    }
+}
