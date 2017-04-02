a42e345abb048f1e09af936cca33250c9afb0deb
PongXD <brendanyhy@gmail.com>
2017-04-02 20:37:18 +0800

    Added Audiotrack methods to play a byte[]

--- CS4347Application/app/src/main/java/cs4347group10/cs4347application/DrumMode.java
+++ CS4347Application/app/src/main/java/cs4347group10/cs4347application/DrumMode.java
@@ -1,18 +1,64 @@
 package cs4347group10.cs4347application;
 
+import android.media.AudioFormat;
+import android.media.AudioManager;
+import android.media.AudioTrack;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
+import android.widget.Button;
+import android.view.View;
+import android.widget.ImageButton;
 
 /**
  * Created by Brendan on 3/29/2017.
  */
 
 public class DrumMode extends AppCompatActivity {
+    /*int samplingRate = 44100;
+    AudioTrack audioTrack;
+    int buffsize;*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drum_mode);
 
+        ImageButton recordingBtn = (ImageButton) findViewById(R.id.record_button);
+        recordingBtn.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        //Start recording
+                    case MotionEvent.ACTION_UP:
+                        //Stop recording and process sound
+                }
+                return true;
+            }
+        });
+
+
     }
+
+    // Not working - will change to an animation
+    public void drumHitAnimation(View view){
+        // Rotate drumsticks
+        view.findViewById(R.id.drumstick1).setRotation(160);
+        view.findViewById(R.id.drumstick2).setRotation(150);
+        // Make hit effect visible
+        view.findViewById(R.id.hit_effect1).setVisibility(View.VISIBLE);
+        view.findViewById(R.id.hit_effect2).setVisibility(View.VISIBLE);
+    }
+
+    /*public void initAudio() {
+        buffsize = AudioTrack.getMinBufferSize(samplingRate, AudioFormat.CHANNEL_OUT_MONO,
+                AudioFormat.ENCODING_PCM_16BIT);
+
+        audioTrack = new AudioTrack(AudioManager.STREAM_MUSIC, samplingRate,
+                AudioFormat.CHANNEL_OUT_MONO,
+                AudioFormat.ENCODING_PCM_16BIT,
+                buffsize,
+                AudioTrack.MODE_STATIC);
+    }*/
 }
--- CS4347Application/app/src/main/java/cs4347group10/cs4347application/PianoMode.java
+++ CS4347Application/app/src/main/java/cs4347group10/cs4347application/PianoMode.java
@@ -1,20 +1,115 @@
 package cs4347group10.cs4347application;
 
 import android.app.Activity;
+import android.media.AudioFormat;
+import android.media.AudioManager;
+import android.media.AudioTrack;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageButton;
 
 /**
  * Created by Brendan on 3/29/2017.
  */
 
 public class PianoMode  extends AppCompatActivity {
-
+    Thread audioThread;
+    int samplingRate = 44100;
+    boolean isRunning = true;
+    AudioTrack audioTrack;
+    int buffsize;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_piano_mode);
 
+        ImageButton recordingBtn = (ImageButton) findViewById(R.id.record_button);
+        ImageButton pianoKey1 = (ImageButton) findViewById(R.id.pianoKey1);
+        ImageButton pianoKey2 = (ImageButton) findViewById(R.id.pianoKey2);
+        ImageButton pianoKey3 = (ImageButton) findViewById(R.id.pianoKey3);
+        ImageButton pianoKey4 = (ImageButton) findViewById(R.id.pianoKey4);
+        ImageButton pianoKey5 = (ImageButton) findViewById(R.id.pianoKey5);
+        ImageButton pianoKey6 = (ImageButton) findViewById(R.id.pianoKey6);
+        ImageButton pianoKey7 = (ImageButton) findViewById(R.id.pianoKey7);
+
+        recordingBtn.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+
+                    case MotionEvent.ACTION_UP:
+
+                }
+                return true;
+            }
+        });
+        pianoKey1.setOnTouchListener(this.onTouchListener);
+        pianoKey2.setOnTouchListener(this.onTouchListener);
+        pianoKey3.setOnTouchListener(this.onTouchListener);
+        pianoKey4.setOnTouchListener(this.onTouchListener);
+        pianoKey5.setOnTouchListener(this.onTouchListener);
+        pianoKey6.setOnTouchListener(this.onTouchListener);
+        pianoKey7.setOnTouchListener(this.onTouchListener);
+
+        audioThread = new Thread() {
+            public void run() {
+                // set process priority
+                setPriority(Thread.MAX_PRIORITY);
+                initAudio();
+                playAudio();
+            }
+        };
+        audioThread.start();
     }
+
+    public View.OnTouchListener onTouchListener = new View.OnTouchListener() {
+        public boolean onTouch(View view, MotionEvent event) {
+            switch (event.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                    isRunning = true;
+                case MotionEvent.ACTION_UP:
+                    isRunning = false;
+            }
+            return true;
+        }
+    };
+
+    public void initAudio() {
+        buffsize = AudioTrack.getMinBufferSize(samplingRate, AudioFormat.CHANNEL_OUT_MONO,
+                AudioFormat.ENCODING_PCM_16BIT);
+
+        audioTrack = new AudioTrack(AudioManager.STREAM_MUSIC, samplingRate,
+                AudioFormat.CHANNEL_OUT_MONO,
+                AudioFormat.ENCODING_PCM_16BIT,
+                buffsize,
+                AudioTrack.MODE_STREAM);
+    }
+
+    public void playAudio() {
+        audioTrack.play();
+        byte[] sound = new byte[buffsize];
+        while(isRunning){
+            for(int i=0; i < buffsize; i++){
+                // Get sound
+            }
+            audioTrack.write(sound, 0, buffsize);
+        }
+        audioTrack.stop();
+    }
+
+    public void onDestroy(){
+        super.onDestroy();
+        isRunning = false;
+        try {
+            audioThread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        audioThread = null;
+    }
+
 }
