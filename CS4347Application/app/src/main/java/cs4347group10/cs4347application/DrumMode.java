988ab03065a891289f3fc016a2137e6e3377d4c0
JEFFERSON\jefferson <jeffersonzx1992@gmail.com>
2017-04-05 14:47:33 +0800

    Drum mode can play music sound w.r.t shaking speed

--- CS4347Application/app/src/main/java/cs4347group10/cs4347application/DrumMode.java
+++ CS4347Application/app/src/main/java/cs4347group10/cs4347application/DrumMode.java
@@ -9,15 +9,48 @@ import android.view.MotionEvent;
 import android.widget.Button;
 import android.view.View;
 import android.widget.ImageButton;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.media.MediaPlayer;
+import android.provider.Settings;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Toast;
+import android.content.Context;
+import android.content.Intent;
+import android.media.SoundPool;
+import java.util.*;
 
 /**
  * Created by Brendan on 3/29/2017.
  */
 
-public class DrumMode extends AppCompatActivity {
+
+public class DrumMode extends AppCompatActivity implements SensorEventListener {
+    private static final int SHAKETHRESHOLD = 800;
     /*int samplingRate = 44100;
     AudioTrack audioTrack;
     int buffsize;*/
+
+    private Sensor accelerometer;
+    private long curTime, lastUpdate;
+    private float x,y,z,last_x,last_y,last_z, shake_speed;
+
+    SoundPool mySound;
+    int Shake_id, Shake_id2;
+    private SensorManager mSensorManager;
+
+    private ShakeEventListener mSensorListener;
+    //setVolumeControlStream(AudioManager.STREAM_MUSIC);
+
+    AudioManager audioManager;
+    float curVolume, maxVolume, volume;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +70,20 @@ public class DrumMode extends AppCompatActivity {
                 return true;
             }
         });
+
+        mySound = new SoundPool(1, AudioManager.STREAM_MUSIC, 0);
+        Shake_id = mySound.load(this, R.raw.drumsound, 1);
+        Shake_id2 = mySound.load(this, R.raw.shake,1);
+
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        accelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mSensorManager.registerListener(this,accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        curTime = lastUpdate = (long)0.0;
+        x = y = z = last_x = last_y = last_z = (float)0.0;
+        mSensorListener = new ShakeEventListener();
+
+        volumeSounds();
+
 
 
     }
@@ -61,4 +108,59 @@ public class DrumMode extends AppCompatActivity {
                 buffsize,
                 AudioTrack.MODE_STATIC);
     }*/
+
+    @Override
+    public  void onSensorChanged(SensorEvent event) {
+        long curTime = System.currentTimeMillis();
+
+        if ((curTime - lastUpdate) > 200) {
+            long diffTime = (curTime - lastUpdate);
+            lastUpdate = curTime;
+
+            x = event.values[0];
+            y = event.values[1];
+            z = event.values[2];
+
+            float speed = Math.abs(x + y + z - last_x - last_y - last_z) / diffTime * 10000;
+            shake_speed = speed;
+
+            float volume = (30 - (int) (speed / 100)) % 30;
+
+            volume = (float) (volume / 30.0) * 5;
+
+            if (volume <= 1) {
+                volume = 1;
+            } else if (volume > 1 && volume <= 2) {
+                volume = 1;
+            } else if (volume > 2 && volume <= 3) {
+                volume = 3;
+            } else if (volume > 3 && volume <= 4) {
+                volume = 5;
+            } else if (volume > 4 && volume <= 5) {
+                volume = 5;
+            } else {
+                mySound.play(Shake_id2, 1, 1, 1, 0, 1);
+            }
+
+            System.out.println("volume = " + volume);
+            System.out.println("speed = " + speed);
+
+            if (shake_speed > SHAKETHRESHOLD) {
+                mySound.play(Shake_id, 1 / volume, 1 / volume, 1, 0, 1);
+            }
+
+        }
+    }
+
+    protected void volumeSounds(){
+        audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
+        curVolume = (float)audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+        maxVolume = (float)audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        volume = curVolume / maxVolume;
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    }
+
 }
