0965f13e0fec7787616b00f9244ec3028990b73f
Admin <Admin@r-183-106-25-172.comp.nus.edu.sg>
2017-04-07 17:45:47 +0800

    no message

--- CS4347Application/app/src/main/java/cs4347group10/cs4347application/ShakeEventListener.java
+++ CS4347Application/app/src/main/java/cs4347group10/cs4347application/ShakeEventListener.java
@@ -10,57 +10,26 @@ package cs4347group10.cs4347application;
         import android.hardware.SensorEvent;
         import android.hardware.SensorEventListener;
         import android.hardware.SensorManager;
+        import android.location.Location;
+
 
 /**
  * Listener that detects shake gesture.
  */
 public class ShakeEventListener implements SensorEventListener {
 
+    private long InitalDirectionChangeTime = 0;
+    private long LastDirectionChangeTime;
+    private int NoOfChangeDirection = 0;
 
-    /** Minimum movement force to consider. */
-    private static final int MIN_FORCE = 5;
-
-    /**
-     * Minimum times in a shake gesture that the direction of movement needs to
-     * change.
-     */
-    private static final int MIN_DIRECTION_CHANGE = 3;
-
-    /** Maximum pause between movements. */
-    private static final int MAX_PAUSE_BETHWEEN_DIRECTION_CHANGE = 400;
-
-    /** Maximum allowed time for shake gesture. */
-    private static final int MAX_TOTAL_DURATION_OF_SHAKE = 400;
-
-    /** Time when the gesture started. */
-    private long mFirstDirectionChangeTime = 0;
-
-    /** Time when the last movement started. */
-    private long mLastDirectionChangeTime;
-
-    /** How many movements are considered so far. */
-    private int mDirectionChangeCount = 0;
-
-    /** The last x position. */
     private float lastX = 0;
-
-    /** The last y position. */
     private float lastY = 0;
-
-    /** The last z position. */
     private float lastZ = 0;
 
-    /** OnShakeListener that is called when shake is detected. */
     private OnShakeListener mShakeListener;
 
-    /**
-     * Interface for shake gesture.
-     */
-    public interface OnShakeListener {
 
-        /**
-         * Called when shake gesture is detected.
-         */
+    public interface OnShakeListener {
         void onShake();
     }
 
@@ -69,33 +38,31 @@ public class ShakeEventListener implements SensorEventListener {
     }
 
     @Override
-    public void onSensorChanged(SensorEvent se) {
-        // get sensor data
-        float x = se.values[SensorManager.DATA_X];
-        float y = se.values[SensorManager.DATA_Y];
-        float z = se.values[SensorManager.DATA_Z];
+    public void onSensorChanged(SensorEvent se) {// get sensor data
 
-        // calculate movement
-        float totalMovement = Math.abs(x + y + z - lastX - lastY - lastZ);
+        float x = se.values[0];
+        float y = se.values[1];
+        float z = se.values[2];
 
-        if (totalMovement > MIN_FORCE) {
+
+        if (Math.abs(x + y + z - lastX - lastY - lastZ) > 5) { // force
 
             // get time
-            long now = System.currentTimeMillis();
+            long current = System.currentTimeMillis();
 
             // store first movement time
-            if (mFirstDirectionChangeTime == 0) {
-                mFirstDirectionChangeTime = now;
-                mLastDirectionChangeTime = now;
+            if (InitalDirectionChangeTime == 0) {
+                InitalDirectionChangeTime = current;
+                LastDirectionChangeTime = current;
             }
 
             // check if the last movement was not long ago
-            long lastChangeWasAgo = now - mLastDirectionChangeTime;
-            if (lastChangeWasAgo < MAX_PAUSE_BETHWEEN_DIRECTION_CHANGE) {
+            long lastChangeWasAgo = current - LastDirectionChangeTime;
+            if (lastChangeWasAgo < 400) { //delay time between direction change
 
                 // store movement data
-                mLastDirectionChangeTime = now;
-                mDirectionChangeCount++;
+                LastDirectionChangeTime = current;
+                NoOfChangeDirection++;
 
                 // store last sensor data
                 lastX = x;
@@ -103,32 +70,30 @@ public class ShakeEventListener implements SensorEventListener {
                 lastZ = z;
 
                 // check how many movements are so far
-                if (mDirectionChangeCount >= MIN_DIRECTION_CHANGE) {
+                if (NoOfChangeDirection >= 2) { //changing direction
 
                     // check total duration
-                    long totalDuration = now - mFirstDirectionChangeTime;
-                    if (totalDuration < MAX_TOTAL_DURATION_OF_SHAKE) {
+                    long totalDuration = current - InitalDirectionChangeTime;
+                    if (totalDuration < 400) {
                         mShakeListener.onShake();
-                        resetShakeParameters();
+                        InitalDirectionChangeTime = 0;
+                        NoOfChangeDirection = 0;
+                        LastDirectionChangeTime = 0;
+                        lastX = 0;
+                        lastY = 0;
+                        lastZ = 0;
                     }
                 }
 
             } else {
-                resetShakeParameters();
+                InitalDirectionChangeTime = 0;
+                NoOfChangeDirection = 0;
+                LastDirectionChangeTime = 0;
+                lastX = 0;
+                lastY = 0;
+                lastZ = 0;
             }
         }
-    }
-
-    /**
-     * Resets the shake parameters to their default values.
-     */
-    private void resetShakeParameters() {
-        mFirstDirectionChangeTime = 0;
-        mDirectionChangeCount = 0;
-        mLastDirectionChangeTime = 0;
-        lastX = 0;
-        lastY = 0;
-        lastZ = 0;
     }
 
     @Override
