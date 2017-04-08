67178d4630d8466082bceb935f708307b98533f7
phamthihong <phamthihong292@hotmail.co.uk>
2017-04-08 11:49:27 +0800

    Update ShakeEventListener.java

--- CS4347Application/app/src/main/java/cs4347group10/cs4347application/ShakeEventListener.java
+++ CS4347Application/app/src/main/java/cs4347group10/cs4347application/ShakeEventListener.java
@@ -7,20 +7,14 @@ package cs4347group10.cs4347application;
 
 
         import android.hardware.Sensor;
-        import android.hardware.SensorEvent;
-        import android.hardware.SensorEventListener;
-        import android.hardware.SensorManager;
-        import android.location.Location;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
 
 
 /**
  * Listener that detects shake gesture.
  */
 public class ShakeEventListener implements SensorEventListener {
-
-    private long InitalDirectionChangeTime = 0;
-    private long LastDirectionChangeTime;
-    private int NoOfChangeDirection = 0;
 
     private float lastX = 0;
     private float lastY = 0;
@@ -38,16 +32,19 @@ public class ShakeEventListener implements SensorEventListener {
     }
 
     @Override
-    public void onSensorChanged(SensorEvent se) {// get sensor data
+    public void onSensorChanged(SensorEvent event) {// get sensor data
+        long InitalDirectionChangeTime = 0;
+        long LastDirectionChangeTime =0;
+        int NoOfChangeDirection = 0;
 
-        float x = se.values[0];
-        float y = se.values[1];
-        float z = se.values[2];
+        float x = event.values[0];
+        float y = event.values[1];
+        float z = event.values[2];
 
 
-        if (Math.abs(x + y + z - lastX - lastY - lastZ) > 5) { // force
+        if (Math.abs(x + y + z - lastX - lastY - lastZ) > 5) { // total change in distance
 
-            // get time
+            // get current time
             long current = System.currentTimeMillis();
 
             // store first movement time
@@ -56,29 +53,18 @@ public class ShakeEventListener implements SensorEventListener {
                 LastDirectionChangeTime = current;
             }
 
-            // check if the last movement was not long ago
-            long lastChangeWasAgo = current - LastDirectionChangeTime;
-            if (lastChangeWasAgo < 400) { //delay time between direction change
+            if (current - LastDirectionChangeTime < 400) { //delay time between direction change
 
-                // store movement data
                 LastDirectionChangeTime = current;
                 NoOfChangeDirection++;
-
-                // store last sensor data
                 lastX = x;
                 lastY = y;
                 lastZ = z;
 
-                // check how many movements are so far
                 if (NoOfChangeDirection >= 2) { //changing direction
 
-                    // check total duration
-                    long totalDuration = current - InitalDirectionChangeTime;
-                    if (totalDuration < 400) {
+                    if (current - InitalDirectionChangeTime < 400) {
                         mShakeListener.onShake();
-                        InitalDirectionChangeTime = 0;
-                        NoOfChangeDirection = 0;
-                        LastDirectionChangeTime = 0;
                         lastX = 0;
                         lastY = 0;
                         lastZ = 0;
@@ -86,9 +72,6 @@ public class ShakeEventListener implements SensorEventListener {
                 }
 
             } else {
-                InitalDirectionChangeTime = 0;
-                NoOfChangeDirection = 0;
-                LastDirectionChangeTime = 0;
                 lastX = 0;
                 lastY = 0;
                 lastZ = 0;
